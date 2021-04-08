8bfd0dc5bc276bc406208dde794c5a0de6cb35fe
EvgenySaenko <storn84@mail.ru>
2021-04-08 14:54:38 +0300

    add branch homework1

---
+++ src/main/java/ru/sber/javaschool/homeworks/tasks/task2002/Solution.java
@@ -0,0 +1,16 @@
+package ru.sber.javaschool.homeworks.tasks.task2002;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i < count; i++) {
+            sum += sc.nextInt();
+        }
+
+        System.out.print(sum);
+    }
+}
