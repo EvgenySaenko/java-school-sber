e5b0c68984c276ca2fab6a9dd2a85fc84da0ca1b
EvgenySaenko <storn84@mail.ru>
2021-04-19 17:44:23 +0300

    add homework4 terminal application implemented

---
+++ src/main/java/ru/sber/javaschool/homeworks/homework4/GUI/TerminalController.java
@@ -0,0 +1,147 @@
+package ru.sber.javaschool.homeworks.homework4.GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
+import ru.sber.javaschool.homeworks.homework4.util.Callback;
+import ru.sber.javaschool.homeworks.homework4.TerminalImpl;
+
+//TODO - не обработана ошибка NumberFormatException при распарсивании "" пустого места
+public class TerminalController implements Callback {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button showCash;
+
+    @FXML
+    private Button putCash;
+
+    @FXML
+    private Button getCash;
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    private TextField tfPut;
+
+    @FXML
+    private TextField tfGet;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private Label labelTerminal;
+
+    private TerminalImpl terminal;
+
+    private final double defaultFontSize = 38;
+    private final Font defaultFont = Font.font(defaultFontSize);
+
+    private static boolean successfully = false;
+
+    @Override
+    public boolean callingBack(boolean b) {
+        System.out.println(successfully = b);
+        return successfully = b;
+    }
+
+
+    @FXML
+    void initialize() {
+        textArea.setEditable(false);
+        this.terminal = new TerminalImpl(this);
+
+    }
+
+    public void showNoCorrectEnterGetCash() {//показываем сколько осталось попыток
+        labelTerminal.setText("Введите сумму кратную 100");
+        labelTerminal.setVisible(true);
+    }
+
+    public void showInsufficientFunds(){
+        labelTerminal.setText("Недостаточно средств для снятия такой суммы: " + tfGet.getText() + "\n" + "Проверьте баланс средств!");
+        labelTerminal.setVisible(true);
+        tfGet.clear();
+    }
+
+
+    //при нажатии exit
+    public void btnExit(ActionEvent actionEvent) {
+        terminal.resetOnlineClient();//сброс текущего клиента
+        Platform.exit();
+        System.out.println("Клиент отключился");
+    }
+
+    //посмотреть баланс на счете
+    public void showCash(ActionEvent actionEvent) {
+        System.out.println(terminal.showCash());
+        textArea.setFont(defaultFont);
+        textArea.setText(String.valueOf(terminal.showCash()));
+    }
+
+    //положить деньги
+    public void putCash(ActionEvent actionEvent) {
+        tfPutCash(actionEvent);//когда нажмем положить => вызовим
+    }
+
+    //снять деньги
+    public void getCash(ActionEvent actionEvent) {
+        tfGetCash(actionEvent);//когда нажмем положить => вызовим
+    }
+
+    public void tfGetCash(ActionEvent actionEvent) {
+        System.out.println("successfully до => " + successfully);
+        try {
+            if (Integer.parseInt(tfGet.getText()) % 100 == 0) {
+                terminal.getCash(Integer.parseInt(tfGet.getText()));
+                labelTerminal.setVisible(false);
+                tfGet.clear();
+            }
+            if (Integer.parseInt(tfGet.getText()) % 100 != 0) {
+                showNoCorrectEnterGetCash();
+                tfGet.clear();
+            }
+        } catch (NumberFormatException e){
+            throw new RuntimeException("Не удалось преобразование типов",e);
+        }catch (IllegalArgumentException ex){
+            throw new IllegalArgumentException("Введене превышающее значение средст на счете",ex);
+        }finally{
+            System.out.println("successfully после " + successfully);
+            if (successfully) {
+                successfully = false;
+                showInsufficientFunds();
+            } else {
+                tfGet.clear();
+            }
+        }
+    }
+
+    public void tfPutCash(ActionEvent actionEvent) {
+        if (Integer.parseInt(tfPut.getText()) % 100 == 0) {
+            System.out.println("OKEY");
+            terminal.putCash(Integer.parseInt(tfPut.getText()));
+            labelTerminal.setVisible(false);
+            tfPut.clear();
+        } else if (Integer.parseInt(tfPut.getText()) % 100 != 0) {
+            System.out.println("NOO  OKEY");
+            showNoCorrectEnterGetCash();
+            tfPut.clear();
+        }
+    }
+
+}
---
+++ src/main/java/ru/sber/javaschool/homeworks/homework4/util/PinValidator.java
@@ -0,0 +1,41 @@
+package ru.sber.javaschool.homeworks.homework4.util;
+
+import ru.sber.javaschool.homeworks.homework4.db.DataBase;
+
+import javax.xml.bind.ValidationException;
+
+//проверка валидности данных
+//TODO - проверка если ничего не ввели
+public class PinValidator {
+    private static final int CORRECT_LENGTH_PASSWORD = 4;
+    private static final int INCORRECT_LENGTH_PASSWORD = 0;
+    private static final int HUNDRED = 100;
+
+    private DataBase dataBase;
+
+    public PinValidator(){
+        dataBase = DataBase.getDataBase();//инициализировалли данные
+    }
+
+    public boolean isCorrectPassword(int password){//корректность ввода
+       if (String.valueOf(password).length() != CORRECT_LENGTH_PASSWORD || password == INCORRECT_LENGTH_PASSWORD ||
+               String.valueOf(password).equals("")){
+           try {
+               throw new ValidationException("Введен не корректный ПИН-КОД => " + password);
+           } catch (ValidationException e) {
+               e.printStackTrace();
+           }
+       }
+       return checkPassword(password);//если корректно ввели проверяем в базе
+    }
+
+    public boolean isCorrect(int amount){
+        return amount % HUNDRED == 0;
+    }
+
+
+    public boolean checkPassword(int password){//проверка в базе
+      return dataBase.isExist(password);
+    }
+    
+}
